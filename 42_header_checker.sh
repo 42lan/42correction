e41b99e936e1f471ca0198fdf91c9484361b9e8d
4slan <amalsago@student.42.fr>
2019-11-13 11:51:59 +0100

    printing name of field to distinguish them

--- 42_header_checker.sh
+++ 42_header_checker.sh
@@ -6,10 +6,13 @@
 #    By: amalsago <amalsago@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2019/10/26 20:17:26 by amalsago          #+#    #+#              #
-#    Updated: 2019/10/26 20:18:47 by amalsago         ###   ########.fr        #
+#    Updated: 2019/11/13 11:50:10 by amalsago         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
-grep -r --include \*.c "By:" $@ | awk '{print $3}' | sort | uniq
-grep -r --include \*.c "Created:" $@ | awk '{print $6}' | sort | uniq
-grep -r --include \*.c "Updated:" $@ | awk '{print $6}' | sort | uniq
+echo "By:"
+grep -r --include \*.c "By:" $@ | awk '{print $3}' | sort | uniq; echo
+echo "Created:"
+grep -r --include \*.c "Created:" $@ | awk '{print $6}' | sort | uniq; echo
+echo "Updated:"
+grep -r --include \*.c "Updated:" $@ | awk '{print $6}' | sort | uniq;
