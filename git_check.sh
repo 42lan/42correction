162ce84e5b4bf714261dee371ad784e1c14c8e8f
Aslan MALSAGOV <amalsago@student.42.fr>
2020-04-12 00:50:47 +0200

    add count number of code lines in git repository per user

--- git_check.sh
+++ git_check.sh
@@ -11,3 +11,4 @@
 # **************************************************************************** #
 
 git shortlog -s
+git ls-files | xargs -n1 git blame --line-porcelain | sed -n 's/^author //p' | sort -f | uniq -ic | sort -nr
